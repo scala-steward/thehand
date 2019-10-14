136e6ac3d7c56d61f9c1f2e9917f41b9a19f6f86
Jeison <cardoso.jeison@gmail.com>
2019-10-14 09:20:01 -0300

    change dump route and create custom fields API test

---
+++ test/CustomFieldsApiV1Spec.scala
@@ -0,0 +1,39 @@
+import ApplicationFixture.fixture
+import models.DatabaseSuffix
+import org.specs2.matcher.Scope
+import play.api.mvc.Result
+
+import scala.concurrent.Future
+
+class CustomFieldsApiV1Spec extends ApiSpecification {
+  fixture.populate(DatabaseSuffix("CUSTOM_FIELDS_"))
+  "/api customfields" should {
+    s"return a list of customfields" in new Scope {
+      val result: Future[Result] = routeGET(
+        "/api/v1/CUSTOM_FIELDS_/customfields")
+      status(result) must equalTo(OK)
+      val s = contentAsString(result)
+      s must beEqualTo(
+        s"""[{"fieldValue":"internal","field":"Bug","taskId":1,"id":1}]""").ignoreSpace
+    }
+    s"return a customfields by id" in new Scope {
+      val result: Future[Result] = routeGET(
+        "/api/v1/CUSTOM_FIELDS_/customfields/1")
+      status(result) must equalTo(OK)
+      contentType(result) must beSome.which(_ == "application/json")
+      val s = contentAsString(result)
+      s must beEqualTo(
+        s"""[{"fieldValue":"internal","field":"Bug","taskId":1,"id":1}]""").ignoreSpace
+    }
+    s"return a customfields by field Bug" in new Scope {
+      val result: Future[Result] = routeGET(
+        "/api/v1/CUSTOM_FIELDS_/customfields/field/Bug")
+      status(result) must equalTo(OK)
+      contentType(result) must beSome.which(_ == "application/json")
+      val s = contentAsString(result)
+      s must beEqualTo(
+        s"""[{"fieldValue":"internal","field":"Bug","taskId":1,"id":1}]""").ignoreSpace
+    }
+  }
+
+}
